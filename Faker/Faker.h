a63b17a6a56b24ef5374fd6e2b352223be58a7c9
Romain Pouclet <rpouclet@mirego.com>
2015-08-20 18:54:30 -0400

    Initial Commit

---
+++ Faker/Faker.h
@@ -0,0 +1,19 @@
+//
+//  Faker.h
+//  Faker
+//
+//  Created by Romain Pouclet on 2015-08-20.
+//  Copyright © 2015 Perfectly-Cooked. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Faker.
+FOUNDATION_EXPORT double FakerVersionNumber;
+
+//! Project version string for Faker.
+FOUNDATION_EXPORT const unsigned char FakerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Faker/PublicHeader.h>
+
+
